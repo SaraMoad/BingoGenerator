2704cf0ed16a7ebd0289c30dba14922a5a0722ca
Sara Moad <102942360+SaraMoad@users.noreply.github.com>
2023-03-17 10:15:59 +1100

    BingoGen-resetting the prompt arr with all prompts

--- bingogenerator/src/components/BingoGenerator.jsx
+++ bingogenerator/src/components/BingoGenerator.jsx
@@ -1,16 +1,22 @@
 import { bingoPrompts } from "../data/bingoPrompt";
 
-
-const bingoGenerator = () => {
-    const optionsArray = bingoPrompts;
+let usedOptions = [];
+    
+const BingoGenerator = () => {
+    let optionsArray = bingoPrompts 
     const currentBingoOptions = []
     for (let x = 0; x < 4; x++) {
         const i = Math.floor(Math.random() * optionsArray.length)
         currentBingoOptions.push(optionsArray[i])
-        optionsArray.splice(i, 1)
+        usedOptions.push(optionsArray.splice(i, 1).toString())
     }
-
+    if (optionsArray.length < 4) {
+        for (let i = 0; i < usedOptions.length; i++) {
+        optionsArray.push(usedOptions[i]).toString()
+        }
+        usedOptions = []
+    } 
     return currentBingoOptions;
 };
 
-export default bingoGenerator;
+export default BingoGenerator;
