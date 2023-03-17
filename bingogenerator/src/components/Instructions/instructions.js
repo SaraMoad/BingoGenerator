a89875a241a747f210ccb2d922226c5deb9af797
Sara Moad <102942360+SaraMoad@users.noreply.github.com>
2023-03-17 13:26:03 +1100

    instruction box and styling

--- bingogenerator/src/components/Instructions/instructions.js
+++ bingogenerator/src/components/Instructions/instructions.js
@@ -1,1 +1,13 @@
 // has the prewritten instructions for GPN but create the ability to edit and change the value.
+import './instructions.css'
+
+const instructionString = `Rules: 1) Read each square. 2) Find a new friend who can complete any of the squares. 3) One friend can fill in one square only. 4) The first person to fill in every square wins!`;
+const Instructions = () => {
+   return (
+        <div className='textbox'>
+        <p>{instructionString}</p>
+        </div>
+   ) 
+}
+
+export default Instructions; 
