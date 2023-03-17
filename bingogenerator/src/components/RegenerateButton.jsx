51b08bd3fa61d0af49d8b0f8cb299e6316b9363e
Sara Moad <102942360+SaraMoad@users.noreply.github.com>
2023-03-17 13:26:25 +1100

    styling of button

--- bingogenerator/src/components/RegenerateButton.jsx
+++ bingogenerator/src/components/RegenerateButton.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import '../style.css'
 
-
+// style with gpn green, add a hover component, hide it when printed, increase size and padding. 
 
 const RegenerateButton = ({ regenerate }) => {
   return (
-    <button onClick={regenerate}>
-      Generate
+    <button className="button" onClick={regenerate}>
+      GENERATE
     </button>
   );
 }
