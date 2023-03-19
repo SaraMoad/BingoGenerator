d9fce23114d28268e8ee1e04a64e7698541111ac
Sara Moad <102942360+SaraMoad@users.noreply.github.com>
2023-03-20 10:28:52 +1100

    Adding of Component PromptList

--- bingogenerator/src/App.js
+++ bingogenerator/src/App.js
@@ -3,13 +3,18 @@ import React, { useState } from 'react';
 import BingoGrid from './components/BingoCard';
 import bingoGenerator from './components/BingoGenerator';
 import RegenerateButton from './components/RegenerateButton'
+import { bingoPrompts } from './data/bingoPrompt';
 
+const orignalPrompts = bingoPrompts
 const App = () => {
   
   const [bingoPrompts, setBingoPrompts] = useState([]);
+  const [usedPrompts, setUsedPrompts] = useState([orignalPrompts]);
+
   
   const handleGeneration = () => {
     setBingoPrompts(bingoGenerator())
+    setUsedPrompts()
   }
   console.log(bingoPrompts)
 
@@ -18,6 +23,7 @@ const App = () => {
       <header className="App-header">
         <BingoGrid bingoPrompts={bingoPrompts} />
         <RegenerateButton regenerate={handleGeneration} />
+        <PromptList bingoPrompts={bingoPrompts} usedPrompts={usedPrompts} />
       </header>
     </div>
   );
