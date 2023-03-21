c628fd321317813b3d2ce22b3170f5e269c02d70
Sara Moad <102942360+SaraMoad@users.noreply.github.com>
2023-03-21 15:20:00 +1100

    adding more prompts for testing

--- bingogenerator/src/data/bingoPrompt.js
+++ bingogenerator/src/data/bingoPrompt.js
@@ -1,55 +1,106 @@
+const GPN =[
+    'First time at GPN 💚',
+    'Has been to GPN before 💚',
+    'Brought a friend to GPN 🧑‍🤝‍🧑',
+]
+
+const skills = [
+    "Can fluently speak more than one language 🗣",
+    'Can play chess ♟️',
+    'Is learning  a new language 🗣️',
+    'Can sign their name in Auslan Sign language 👌',
+    'Knows how to Juggle 🤹‍♀️',
+    'Knows a magic trick 🪄',
+    'Plays an instrument 🎶',
+    'Can ride a skateboard 🛹',
+    'Plays sport on the weekend ⚽️',
+     'test 1',
+    'test 2',
+
+]
+
+const interests = [
+    'Loves doing science experiments 🔬',
+    'Loves solving Puzzles 🧩',
+    'Plays Minecraft  ⛏', 
+    'Writes stories for fun  📝',
+    'Likes to perform Karaoke 🎤',
+    'Loves board games 🎲',
+    'Likes to play Tetris 🎮',
+    'Likes to paint 🎨',
+    'Likes to read 📖',
+     'test 1',
+    'test 2',
+    
+]
+
+const experiences = [
+    'Has visited another country ✈️',
+    'Has touched Snow ❄️',
+    'Has tried something new recently 👣',
+    'Has been camping ⛺',
+    'Has done volunteer work 🦸',
+    'Has performed in public 🎸',
+    'Has done rock climbing 🧗',
+     'test 1',
+    'test 2',
+    'test 3',
+    'hi',
+]
+
+const coding = [
+    'Has coded a game 💻',
+    'Has created their own coding project 💻',
+    'Has worked with others to build a coding project'
+] 
+
+const food = [
+    'Knows what a Kumquat is ❓',
+    'Has made their own pasta 🍝',
+    'Has eaten Dragon fruit 🐲',
+    'Loves to bake 🧁',
+
+]
+
+const favourites = [
+    'Favourite food is Pizza 🍕',
+    'Favourite subject is Maths',
+    'Favourite dessert is brownies',
+]
+
+const similarities = [
+    'Has the same name as you 🪪',
+    'Has the same favourite food as you 🍕',
+    'Has the same favourite animal as you 🐼',
+    'Has the same favourite colour as you 🌈',
+    'Has the same eye colour as you 👀',
+    'Is in the same school year as you 🚌',
+     'test 1',
+    'test 2',
+    'test 3',
+     'test 1',
+]
+
+
+const personal = [
+    'Is wearing cool socks 🧦',
+    'Has more than 1 pet 🐶',
+    'Was born in another country 🌎',
+    'Has lived in a different state 🏠',
+    'Has over 6 letters in their first name 🔤',
+    'Has broken a bone 🦴',
+    'Is an only child 👨‍👩‍👧',
+    'Has a part time or casual job 💰',
+    'Knows the second verse of the national anthem 🇦🇺',
+    'test 1',
+    'test 2',
+    'test 3',
+
+]
 export const bingoPrompts =
-    [
-        "Can fluently speak more than one language 🗣",
-        'Has been to GPN before 💚',
-        'Has visited another country ✈️',
-        'Can play chess ♟️',
-        'Has more than 1 pet 🐶',
-        'Favourite food is Pizza 🍕',
-        'Knows what a Kumquat is ❓',
-        'Has made their own pasta 🍝',
-        'Has eaten Dragon fruit 🐲',
-        'Was born in another country 🌎',
-        'Loves doing science experiments 🔬',
-        'First time at GPN 💚',
-        'Has the same name as you 🪪',
-        'Has touched Snow ❄️',
-        'Has tried something new recently 👣',
-        'Loves to bake 🧁',
-        'Is learning  a new language 🗣️',
-        'Has been camping ⛺',
-        'Has the same favourite food as you 🍕',
-        'Has done volunteer work 🦸',
-        'Loves solving Puzzles 🧩',
-        'Plays Minecraft  ⛏', 
-        'Has lived in a different state 🏠',
-        'Writes stories for fun  📝',
-        'Has performed in public 🎸',
-        'Is wearing cool socks 🧦',
-        'Brought a friend to GPN 🧑‍🤝‍🧑',
-        'Can sign their name in Auslan Sign language 👌',
-        'Likes to perform Karaoke 🎤',
-        'Has over 6 letters in their first name 🔤',
-        'Loves board games 🎲',
-        'Has the same favourite animal as you 🐼',
-        'Has done rock climbing 🧗',
-        'Has coded a game 💻',
-        'Has broken a bone 🦴',
-        'Knows how to Juggle 🤹‍♀️',
-        'Likes to play Tetris 🎮',
-        'Is an only child 👨‍👩‍👧',
-        'Likes to paint 🎨',
-        'Knows a magic trick 🪄',
-        'Plays an instrument 🎶',
-        'Can ride a skateboard 🛹',
-        'Likes to read 📖',
-        'Has the same favourite colour as you 🌈',
-        'Has the same eye colour as you 👀',
-        'Has a part time or casual job 💰',
-        'Is in the same school year as you 🚌',
-        'Knows the second verse of the national anthem 🇦🇺',
-        'Has created their own coding project 💻',
-        'Plays sport on the weekend ⚽️',
-    ]
+    [ skills, interests, experiences, similarities, personal, GPN, favourites, coding, food]
+        
+        
+    
 
     console.log(bingoPrompts)
