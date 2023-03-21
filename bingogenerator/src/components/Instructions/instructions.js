b5252011d5ceb3170776bd824fd9b1ec840998f2
Sara Moad <102942360+SaraMoad@users.noreply.github.com>
2023-03-21 15:20:44 +1100

    changing instructions from a varible into p tag

--- bingogenerator/src/components/Instructions/instructions.js
+++ bingogenerator/src/components/Instructions/instructions.js
@@ -1,11 +1,11 @@
 // has the prewritten instructions for GPN but create the ability to edit and change the value.
 import './instructions.css'
 
-const instructionString = `Rules: 1) Read each square. 2) Find a new friend who can complete any of the squares. 3) One friend can fill in one square only. 4) The first person to fill in every square wins!`;
+
 const Instructions = () => {
    return (
         <div className='textbox'>
-        <p>{instructionString}</p>
+        <p>Rules: <b>1)</b> Read each square. <b>2)</b> Find a new friend who can complete any of the squares. <b>3)</b> One friend can fill in <b>one square only. 4)</b> The first person to fill in <b>every square</b> wins!</p>
         </div>
    ) 
 }
