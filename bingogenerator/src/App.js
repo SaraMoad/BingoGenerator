0b7cb41bb80d0ff46d7a269e5025f245fb991532
Sara Moad <102942360+SaraMoad@users.noreply.github.com>
2023-03-29 10:26:30 +1100

    creating an empty bingo card

--- bingogenerator/src/App.js
+++ bingogenerator/src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import BingoGrid from './components/bingoCard/BingoCard';
 import bingoGenerator from './components/bingoCard/BingoGenerator'
 import RegenerateButton from './components/bingoCard/RegenerateButton'
+import PromptList from './components/Prompt List/promptList';
 import { bingoPrompts } from './data/bingoPrompt';
 
 const orignalPrompts = bingoPrompts
@@ -11,10 +12,17 @@ const App = () => {
   const [bingoPrompts, setBingoPrompts] = useState([]);
   const [usedPrompts, setUsedPrompts] = useState([orignalPrompts]);
 
-  
+  useEffect(() => {
+    const emptyBingoCard = []
+  for (let i = 0; i < 25; i++) {
+    emptyBingoCard.push("")
+    }
+    setBingoPrompts(emptyBingoCard)
+  }, [])
+
   const handleGeneration = () => {
     setBingoPrompts(bingoGenerator())
-    setUsedPrompts()
+    setUsedPrompts(bingoPrompts)
   }
   console.log(bingoPrompts)
 
@@ -22,8 +30,8 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <BingoGrid bingoPrompts={bingoPrompts} />
+        <PromptList usedPrompts={usedPrompts} />
         <RegenerateButton regenerate={handleGeneration} />
-        {/* <PromptList bingoPrompts={bingoPrompts} usedPrompts={usedPrompts} /> */}
       </header>
     </div>
   );
