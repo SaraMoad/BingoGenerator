b7e0ecdea761a99292a928dfbaa1e486e67b9a27
Sara Moad <102942360+SaraMoad@users.noreply.github.com>
2023-05-03 16:36:02 +1000

    css adjustments for alignment of page & new prompt

--- bingogenerator/src/data/bingoPrompt.js
+++ bingogenerator/src/data/bingoPrompt.js
@@ -1,106 +1,105 @@
-const GPN =[
-    'First time at GPN 💚',
-    'Has been to GPN before 💚',
-    'Brought a friend to GPN 🧑‍🤝‍🧑',
-]
+const GPN = [
+  "First time at GPN 💚",
+  "Has been to GPN before 💚",
+  "Brought a friend to GPN 🧑‍🤝‍🧑",
+];
 
 const skills = [
-    "Can fluently speak more than one language 🗣",
-    'Can play chess ♟️',
-    'Is learning  a new language 🗣️',
-    'Can sign their name in Auslan Sign language 👌',
-    'Knows how to Juggle 🤹‍♀️',
-    'Knows a magic trick 🪄',
-    'Plays an instrument 🎶',
-    'Can ride a skateboard 🛹',
-    'Plays sport on the weekend ⚽️',
-     'test 1',
-    'test 2',
-
-]
+  "Can fluently speak more than one language 🗣",
+  "Can play chess ♟️",
+  "Is learning  a new language 🗣️",
+  "Can sign their name in Auslan Sign language 👌",
+  "Knows how to Juggle 🤹‍♀️",
+  "Knows a magic trick 🪄",
+  "Plays an instrument 🎶",
+  "Can ride a skateboard 🛹",
+  "Plays sport on the weekend ⚽️",
+  "Can say the alphabet backwards",
+  "nn",
+];
 
 const interests = [
-    'Loves doing science experiments 🔬',
-    'Loves solving Puzzles 🧩',
-    'Plays Minecraft  ⛏', 
-    'Writes stories for fun  📝',
-    'Likes to perform Karaoke 🎤',
-    'Loves board games 🎲',
-    'Likes to play Tetris 🎮',
-    'Likes to paint 🎨',
-    'Likes to read 📖',
-     'test 1',
-    'test 2',
-    
-]
+  "Loves doing science experiments 🔬",
+  "Loves solving Puzzles 🧩",
+  "Plays Minecraft  ⛏",
+  "Writes stories for fun  📝",
+  "Likes to perform Karaoke 🎤",
+  "Loves board games 🎲",
+  "Likes to play Tetris 🎮",
+  "Likes to paint 🎨",
+  "Likes to read 📖",
+  "Likes to dance",
+  "Likes to garden",
+];
 
 const experiences = [
-    'Has visited another country ✈️',
-    'Has touched Snow ❄️',
-    'Has tried something new recently 👣',
-    'Has been camping ⛺',
-    'Has done volunteer work 🦸',
-    'Has performed in public 🎸',
-    'Has done rock climbing 🧗',
-     'test 1',
-    'test 2',
-    'test 3',
-    'hi',
-]
+  "Has visited another country ✈️",
+  "Has touched Snow ❄️",
+  "Has tried something new recently 👣",
+  "Has been camping ⛺",
+  "Has done volunteer work 🦸",
+  "Has performed in public 🎸",
+  "Has done rock climbing 🧗",
+  "Has gone on a hike",
+  "Has been ice-skating",
+  "Has gone surfing",
+  "has gone horse riding",
+];
 
 const coding = [
-    'Has coded a game 💻',
-    'Has created their own coding project 💻',
-    'Has worked with others to build a coding project'
-] 
+  "Has coded a game 💻",
+  "Has created their own coding project 💻",
+  "Has worked with others to build a coding project",
+];
 
 const food = [
-    'Knows what a Kumquat is ❓',
-    'Has made their own pasta 🍝',
-    'Has eaten Dragon fruit 🐲',
-    'Loves to bake 🧁',
-
-]
+  "Knows what a Kumquat is ❓",
+  "Has made their own pasta 🍝",
+  "Has eaten Dragon fruit 🐲",
+  "Loves to bake 🧁",
+];
 
 const favourites = [
-    'Favourite food is Pizza 🍕',
-    'Favourite subject is Maths',
-    'Favourite dessert is brownies',
-]
+  "Favourite food is Pizza 🍕",
+  "Favourite subject is Maths",
+  "Favourite dessert is brownies",
+];
 
 const similarities = [
-    'Has the same name as you 🪪',
-    'Has the same favourite food as you 🍕',
-    'Has the same favourite animal as you 🐼',
-    'Has the same favourite colour as you 🌈',
-    'Has the same eye colour as you 👀',
-    'Is in the same school year as you 🚌',
-     'test 1',
-    'test 2',
-    'test 3',
-     'test 1',
-]
-
+  "Has the same name as you 🪪",
+  "Has the same favourite food as you 🍕",
+  "Has the same favourite animal as you 🐼",
+  "Has the same favourite colour as you 🌈",
+  "Has the same eye colour as you 👀",
+  "Is in the same school year as you 🚌",
+  "Has the same favourite school subject as you",
+  "Plays the same sport as you",
+  "Has the same birth month as you",
+  "test 1",
+];
 
 const personal = [
-    'Is wearing cool socks 🧦',
-    'Has more than 1 pet 🐶',
-    'Was born in another country 🌎',
-    'Has lived in a different state 🏠',
-    'Has over 6 letters in their first name 🔤',
-    'Has broken a bone 🦴',
-    'Is an only child 👨‍👩‍👧',
-    'Has a part time or casual job 💰',
-    'Knows the second verse of the national anthem 🇦🇺',
-    'test 1',
-    'test 2',
-    'test 3',
-
-]
-export const bingoPrompts =
-    [ skills, interests, experiences, similarities, personal, GPN, favourites, coding, food]
-        
-        
-    
-
-    console.log(bingoPrompts)
+  "Is wearing cool socks 🧦",
+  "Has more than 1 pet 🐶",
+  "Was born in another country 🌎",
+  "Has lived in a different state 🏠",
+  "Has over 6 letters in their first name 🔤",
+  "Has broken a bone 🦴",
+  "Is an only child 👨‍👩‍👧",
+  "Has a part time or casual job 💰",
+  "Knows the second verse of the national anthem 🇦🇺",
+  "Has siblings",
+  "Has coloured hair",
+  "kk",
+];
+export const bingoPrompts = [
+  skills,
+  interests,
+  experiences,
+  similarities,
+  personal,
+  GPN,
+  favourites,
+  coding,
+  food,
+];
